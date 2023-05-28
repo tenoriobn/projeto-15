89aa7f955ec0057712377bbe9dad7b0cb1032f44
Bruno Tenório <tenoriobn@gmail.com>
2023-05-28 13:32:00 -0300

    Removendo arial-label do span

--- assets/js/1-script.js
+++ assets/js/1-script.js
@@ -46,7 +46,6 @@ function createCard() {
                         // Criar o elemento <button> com a classe "card__btn"
                         const cardBtn = document.createElement('span');
                         cardBtn.classList.add('card__btn');
-                        cardBtn.setAttribute('arial-label', 'options');
             
                         // Criar o elemento <div> com a classe "card__body"
                         const cardBody = document.createElement('div');
